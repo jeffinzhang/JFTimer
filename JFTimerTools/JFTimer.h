64492000150bb82a77ba5f8cf7495b9b0944d192
jeffinzhang <jeffinzhang@sina.com>
2019-01-29 11:43:27 +0800

    add pod files

---
+++ JFTimerTools/JFTimer.h
@@ -0,0 +1,19 @@
+//
+//  JFTimer.h
+//  GCD定时器
+//
+//  Created by 张建飞 on 2019/1/25.
+//  Copyright © 2019 张建飞. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JFTimer : NSObject
+
++ (void)cancelTask;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JFTimerTools/JFTimer.m
@@ -0,0 +1,18 @@
+//
+//  JFTimer.m
+//  GCD定时器
+//
+//  Created by 张建飞 on 2019/1/25.
+//  Copyright © 2019 张建飞. All rights reserved.
+//
+
+#import "JFTimer.h"
+
+@implementation JFTimer
+
++ (void)cancelTask
+{
+    NSLog(@"取消哦啊跑了");
+}
+
+@end
